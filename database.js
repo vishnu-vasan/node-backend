ee3fdb1b62188d59f0dbcd3037839aa76b576cfe
Vishnu <vasan2001@gmail.com>
2019-12-11 15:36:21 +0530

    databasepool.js

--- database.js
+++ database.js
@@ -1,73 +1,103 @@
 var mysql = require('mysql');
 const express = require('express');
 const app = express();
-const port = 3000;
 const bodyParser = require('body-parser');
-app.use(bodyParser());
-const fs = require("fs");
+const { uuid } = require('uuidv4');
 
-var con = mysql.createConnection({
+const port = 3000;
+app.use(bodyParser());
+
+let pool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "",
     database: "vishnu"
 });
 
-
-app.get('/get-users-from-json-file/:id', (req, res) => {
-    con.connect(function (err){
-        if(err) console.log(err);
-        console.log(req.params);
-    con.query(`SELECT * FROM USER`,function (err,result){
-        if(err) console.log(err);
-        console.log(result);
-        res.send();
+app.post('/authenticate', function (req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) res.status(500).send('DB connection error');
+        connection.query(`SELECT * FROM USER WHERE EMAIL = "${req.body.email}" AND PASSWORD = "${req.body.password}"`, function (error, results, fields) {
+            connection.release();
+            if (error) res.status(500).send(error);
+            if (results.length > 0) {
+                res.send({ "EMAIL": req.body.email, "UUID TOKEN": uuid() });
+            }
+            else {
+                res.status(401).send({ message: `authorization error,INVALID PASSWORD FOR EMAIL- ${req.body.email}` });
+            }
+        });
     });
 });
+
+app.get('/users', (req, res) => {
+    pool.getConnection(function (err, connection) {
+        if (err) res.status(500).send("DB connection error");
+        connection.query(`SELECT * FROM USER`, function (error, results, fields) {
+            connection.release();
+            if (error) {
+                console.log(error);
+                res.status(500).send(error);
+            }
+            res.send(results);
+        });
+    });
 });
-app.post('/user/:id', function (req, res) {
-    con.connect(function(err){
-        if(err) console.log(err);
-        let val=[[req.body.id,req.body.name,req.body.email,req.body.password]];
-        con.query("INSERT INTO USER VALUES ?",[val],function (err,result){
-            if(err) console.log(err);
-            console.log(result);
-            res.send();
+
+app.post('/user', function (req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) res.status(500).send("DB connection error");
+        let val = [[req.body.name, req.body.email, req.body.password]];
+        connection.query("INSERT INTO USER (name,email,password) VALUES ?", [val], function (error, results, fields) {
+            connection.release();
+            if (error) {
+                console.log(error);
+                res.status(500).send(error);
+            }
+            res.send(results);
         });
     });
 });
 
 app.delete('/user/:id', function (req, res) {
-    con.connect(function (err) {
+    pool.getConnection(function (err, connection) {
         if (err) throw err;
-        con.query(`DELETE FROM USER WHERE id = ${req.params.id}`, function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.send();
+        connection.query(`DELETE FROM USER WHERE id = ${req.params.id}`, function (error, results, fields) {
+            connection.release();
+            if (error) res.status(500).send(error);
+            res.send(results);
         });
     });
 });
+
 app.put('/user/:id', function (req, res) {
-    con.connect(function (err) {
-        if (err) throw err;
-        con.query(`UPDATE USER SET ID = ${req.params.id},NAME = ${JSON.stringify(req.body.name)},EMAIL = ${JSON.stringify(req.body.email)},PASSWORD = ${JSON.stringify(req.body.password)} WHERE id = ${req.params.id}`, function (err, result) {
-           if(err) console.log(err);
-            console.log(result);
-            res.send();
+    pool.getConnection(function (err, connection) {
+        if (err) res.status(500).send("DB connection error");
+        connection.query(`UPDATE USER SET ID = ${req.params.id},NAME = ${JSON.stringify(req.body.name)},EMAIL = ${JSON.stringify(req.body.email)},PASSWORD = ${JSON.stringify(req.body.password)} WHERE id = ${req.params.id}`, function (error, results, fields) {
+            connection.release();
+            if (error) {
+                console.log(error);
+                res.status(500).send(error);
+            }
+            res.send(results);
         });
     });
 
 });
-app.get('/get-users-from-json-file/:id', (req, res) => {
-    con.connect(function (err){
-        if(err) console.log(err);
+
+app.get('/user/:id', (req, res) => {
+    pool.getConnection(function (err, connection) {
+        if (err) res.status(500).send("DB connection error");
         console.log(req.params.id);
-    con.query(`SELECT * FROM USER WHERE id = ${req.params.id}`,function (err,result){
-        if(err) console.log(err);
-        console.log(result)
-        res.send();
+        connection.query(`SELECT * FROM USER WHERE id = ${req.params.id}`, function (error, results, fields) {
+            connection.release();
+            if (error) {
+                console.log(error);
+                res.status(500).send(error);
+            }
+            res.send(results[0]);
+        });
     });
-});
 });
 
 app.listen(port, () => console.log(`example app listening on port ${port}!`));
